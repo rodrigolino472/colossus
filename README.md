d23f54c702dff49bbde5bd3fd56baf9e7ebb9068
Rodrigo Lino <rodrigolino472@gmail.com>
2022-03-28 12:41:58 -0300

    resolvendo bugs

---
+++ README.md
@@ -0,0 +1,3 @@
+Página do jogo Shadow of the colossus, criado para praticar animações e transições e css
+
+link: https://rodrigolino472.github.io/colossus/
--- script.js
+++
@@ -1,5 +0,0 @@
-var hamburguer = document.querySelector(".hamburguer-menu")
-
-hamburguer.addEventListener("click", ()=>{
-  document.querySelector(".nav-menu").classList.toggle("show-menu");
-});
